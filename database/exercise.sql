c8c2c18137b3e9ff30a12421699dba90f8787afe
data-seed.tech <adrian.ionescu@data-seed.tech>
2023-02-15 23:03:06 +0200

    collations

--- database/exercise.sql
+++ database/exercise.sql
@@ -1,23 +1,23 @@
 CREATE TABLE `customers` (
-  `customerId` int(11) NOT NULL AUTO_INCREMENT,
+  `customerId` int NOT NULL AUTO_INCREMENT,
   `customerName` varchar(50) NOT NULL,
   PRIMARY KEY (`customerId`),
   UNIQUE KEY `uniqueCustomer` (`customerName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Customers table for invoicing App exercise';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT='Customers table for invoicing App exercise';
 
 INSERT INTO customers (customerId, customerName)
 VALUES (default, 'AAA. Inc.'), (default, 'BBB Inc.');
 
 
 CREATE TABLE `invoices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `invoiceNumber` varchar(10) NOT NULL,
   `InvoiceDate` date NOT NULL,
-  `customerId` int(11) NOT NULL,
+  `customerId` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `customer` (`customerId`),
   CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customers` (`customerId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Exercise: creating invoicing App';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT='Exercise: creating invoicing App';
 
 
 INSERT INTO invoices (id, invoiceNumber, InvoiceDate, customerId)
@@ -26,15 +26,15 @@ VALUES (default, '123', '2021-08-15', '1'), (default, '234', '2021-08-15', '2');
 
 
 CREATE TABLE `invoice_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `invoiceId` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `invoiceId` int NOT NULL,
   `productName` varchar(100) NOT NULL,
   `quantity` decimal(10,0) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `invoice` (`invoiceId`),
   CONSTRAINT `invoice_details_ibfk_1` FOREIGN KEY (`invoiceId`) REFERENCES `invoices` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Invoices table for invoicing App exercise';
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COMMENT='Invoices table for invoicing App exercise';
 
 
 INSERT INTO invoice_details (id, invoiceId, productName, quantity, price)
@@ -53,5 +53,3 @@ VALUES (default, 'invoices', 'Invoices', 'invoicing'),
 
 INSERT INTO seed_nomenclatures (nomenclatureId, tableName, filterText, appCode)
 VALUES (default, 'customers', 'Customers', 'invoicing');
-
-
